405e523c009a6146aea5cc2d4a82f41922bf9b1a
Goyacyt <1412974228@qq.com>
2023-05-17 17:56:15 +0800

    merge three pattern

--- answerAnalysis.py
+++ answerAnalysis.py
@@ -6,16 +6,18 @@ parser.add_argument("-f","--filename",default="output.txt",help="file to record 
 args=parser.parse_args()
 
 def answerAnalysis(args):
-    f=open ('infothreshold.json','r',encoding='utf-8')
+    """f=open ('infopro0.json','r',encoding='utf-8')
     s=f.read()
-    ff=open ('infothreshold1500.json','r',encoding='utf-8')
+    ff=open ('infopro172.json','r',encoding='utf-8')
     ss=ff.read()
-    result=s+ss
-    """f=open (args.filename,'r',encoding='utf-8')
-    result=f.read()
-    f.close()
-    """
-    wf=open ('infothreshold_03.json','w+',encoding='utf-8')
+    fff=open ('infopro253.json','r',encoding='utf-8')
+    sss=fff.read()
+    ffff=open ('infopro1001.json','r',encoding='utf-8')
+    ssss=ffff.read()
+    fffff=open ('infopro1465.json','r',encoding='utf-8')
+    sssss=fffff.read()
+    result=s+ss+sss+ssss+sssss
+    wf=open ('infoproportion_04_50.json','w+',encoding='utf-8')
     result=result.split('}{')
     for i in range(len(result)):
         if i==0:
@@ -26,7 +28,8 @@ def answerAnalysis(args):
             result[i]='{'+result[i]+'}'
         result[i]=ast.literal_eval(result[i])
     #print(result[0]['groundTruth'],result[0]['answer'])
-    print(result,file=wf)
+    print(result,file=wf)"""
+    
 
 if __name__=='__main__':
     answerAnalysis(args)
